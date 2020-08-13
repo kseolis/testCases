fcff1dd11c2c3ee71934bf656274f6d7abc9f8df
kceo <justkceo@gmail.com>
2020-08-13 13:48:40 +0700

    fix assert in admin page, assertions text in link

--- src/test/java/doublePagesTest.java
+++ src/test/java/doublePagesTest.java
@@ -3,12 +3,18 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.admin.AdminAuthorisationPage;
+import pages.admin.AdminClientPage;
+import pages.admin.AdminMainPage;
 import pages.client.AddressesPage;
 import pages.client.BasePage;
 import pages.client.PassportPage;
 import pages.client.PersonalPage;
 
-import static helpers.TestData.acceptPhoneData;
+import static helpers.TestData.*;
+import static pages.admin.AdminAuthorisationPage.*;
+import static pages.admin.AdminClientPage.*;
+import static pages.admin.AdminMainPage.*;
 import static pages.client.AddressesPage.*;
 import static pages.client.BasePage.*;
 import static pages.client.PassportPage.*;
@@ -28,7 +34,7 @@ public class doublePagesTest {
     }
 
     @Test(description = "Тест СНИЛС идентификации", priority = 1)
-    public void testSnils() throws InterruptedException {
+    public void testSnils() {
         // Первый шаг регистрации. Данные клиента(ФИО, email, телефон);
         openMainPage(driver);
         clickButton(driver, getLoan);
@@ -36,20 +42,23 @@ public class doublePagesTest {
         inputClientData();
         clickButton(driver, acceptAgreement);
         clickButton(driver, proceedButtonPersonalPage);
+
         // Подтверждение номера телефона;
         waitPhoneVerification(driver);
         inputData(code, acceptPhoneData);
         clickButton(driver, acceptPhoneButton);
+
         // Второй шаг регистрации. Паспортные данные клиента;
         waitPassportPageHeading(driver);
         inputClientPassportData(driver);
         waitProceedButtonPassportPage(driver);
         clickButton(driver, proceedButtonPassportPage);
+
         // Третий шаг регистрации. Адрес клиента и доп.контакты;
         waitAddressPageHeading(driver);
         inputAddressesAndAdditionalContact();
 
-/*        // Открытие второго браузера, на стороне админа;
+        // Открытие второго браузера, на стороне админа;
         WebDriver tempDriver = basePage.initializeDriver();
         AdminAuthorisationPage login = new AdminAuthorisationPage(tempDriver);
         AdminMainPage mainPage = new AdminMainPage(tempDriver);
@@ -65,8 +74,7 @@ public class doublePagesTest {
         waitAdminClientsPage(tempDriver);
         inputData(searchField, fullName);
         assertSeeClient(tempDriver);
-        tempDriver.close();*/
-
+        tempDriver.close();
     }
 
     @AfterMethod
